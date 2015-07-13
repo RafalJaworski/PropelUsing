aa967080c88cd13e0e0b6550f00f786bb7708815
Jaworski <r.jaworski@tourmedica.pl>
2015-07-13 22:53:24 +0200

    ProductBundle generated

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Propel\PropelBundle\PropelBundle(),
+            new ProductBundle\ProductBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
