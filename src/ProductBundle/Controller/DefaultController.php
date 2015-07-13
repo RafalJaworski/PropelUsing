75923bf2c302bb8a018242b79f07d9e938ba71d1
Jaworski <r.jaworski@tourmedica.pl>
2015-07-13 23:25:24 +0200

    Add product Action created

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace ProductBundle\Controller;
 
+
+use ProductBundle\Model\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,5 +18,22 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     *
+     * @Route("/add", name="add_product")
+     */
+    public function addAction()
+    {
+        $product = new Product();
+        $product->setName('pilka');
+        $product->setPrice('19,99');
+        $product->setDescription('lalalala');
+
+        $product->save();
+
+        return new Response('Created product '.$product->getId());
+
     }
 }
